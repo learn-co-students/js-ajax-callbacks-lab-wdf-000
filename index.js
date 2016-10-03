7344d7c21724f3da8b94f9b36edda167745c83fe
ckshei <cks856@gmail.com>
2016-10-03 16:36:45 -0400

    Done.

--- index.js
+++ index.js
@@ -6,3 +6,32 @@ function handlebarsSetup() {
 $(document).ready(function (){
   handlebarsSetup()
 });
+
+
+function displayError() {
+  $("#errors").html("I'm sorry, there's been an error. Please try again.")
+}
+
+function searchRepositories() {
+  var searchTerm = $("#searchTerms").val();
+  var url = `https://api.github.com/search/repositories?q=${searchTerm}`
+
+  $.get(url, function(data) {
+    const template = Handlebars.compile($("#results-template").html());
+    $("#results").html(template(data))
+  }).fail( error => {
+    displayError();
+    console.log("failed");
+  })
+
+}
+function showCommits(el) {
+  const owner = el.dataset.owner
+  const repo = el.dataset.repository
+  $.get(`https://api.github.com/repos/${owner}/${repo}/commits`, data => {
+    const template = Handlebars.compile($('#commits-template').html())
+    $('#details').html(template(data))
+  }).fail(error => {
+    displayError()
+  })
+}
