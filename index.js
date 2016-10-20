5400661d528671ec35298a47d3191aef5aa2f9a7
ozPop <ozziepopovas@gmail.com>
2016-10-20 14:45:24 -0400

    add solution

--- index.js
+++ index.js
@@ -4,5 +4,55 @@ function handlebarsSetup() {
 }
 
 $(document).ready(function (){
-  handlebarsSetup()
+  handlebarsSetup();
+  searchClickEvent();
+  commitsClickEvent();
 });
+
+function searchClickEvent() {
+  $('#search').on('click', searchRepositories);
+}
+
+function commitsClickEvent() {
+  // for dynamically added anchor link
+  $('#results').on('click', '#commits', function(e){
+    e.preventDefault();
+    searchCommits($(this).attr('href').split('{')[0]);
+  });
+}
+
+function searchRepositories() {
+  let terms = $('#searchTerms').val();
+  // handle spaces in search terms
+  let searchTerms = terms.split(' ').join('+');
+  const url = "https://api.github.com/search/repositories?q=" + searchTerms;
+  $.get(url, function(data) {
+    // display error if no results
+    if(data.items.length === 0) {
+      displayError();
+    } else {
+      // search results retured something
+      let template = $('#results-template').html();
+      let templateScript = Handlebars.compile(template);
+      let html = templateScript(data);
+      $('#results').append(html);
+    }
+  });
+}
+
+function searchCommits(url) {
+  $.get(url, function(data) {
+    if(data.length === 0) {
+      displayError();
+    } else {
+      let template = $('#commits-template').html();
+      let templateScript = Handlebars.compile(template);
+      let html = templateScript(data);
+      $('#details').append(html);
+    }
+  });
+}
+
+function displayError() {
+  $('#errors').text("I'm sorry, there's been an error. Please try again.")
+}
