e1ec2e95e2e08ca1d2dd5ae65d09aed08a5025a8
Hafizur Rahman <hafuzur.rahmanc@gmail.com>
2016-11-01 19:18:39 -0400

    All tests are passing except showCommit test

--- index.js
+++ index.js
@@ -4,5 +4,46 @@ function handlebarsSetup() {
 }
 
 $(document).ready(function (){
-  handlebarsSetup()
+  handlebarsSetup();
+  searchClickEvent();
+  commitClickEvent();
 });
+
+function searchClickEvent() {
+  $('#search').on('click', searchRepositories);
+}
+
+function commitClickEvent() {
+  $('#results').on('click', '#commits', function(event) {
+
+    event.preventDefault();
+    showCommits($(this).attr('href').split('{')[0]);
+  });
+}
+
+function searchRepositories() {
+  const searchTerms = $('#searchTerms').val().split(' ').join('+');
+  const url = "https://api.github.com/search/repositories?q=" + searchTerms;
+  $.get(url, function(data) {
+    let template = $('#results-template').html();
+    let templateScript = Handlebars.compile(template);
+    $('#results').html(templateScript(data));
+  
+  }).fail(function(error) {
+    displayError();
+  });
+}
+
+function showCommits(url) {
+  $.get(url, function(data) {
+    let template = $('#commits-template').html();
+    let templateScript = Handlebars.compile(template);
+    
+    $('#details').html(templateScript(data));
+  }).fail(function(error) {
+    displayError();
+}); 
+}
+function displayError() {
+  return $('#errors').html("I'm sorry, there's been an error. Please try again.");
+}
