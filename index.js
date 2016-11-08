226be4b05e114586dc7cb1d835f7a522d0342d61
Isuru <github email address>
2016-11-08 13:27:29 -0500

    done

--- index.js
+++ index.js
@@ -1,8 +1,39 @@
+
+function displayError() {
+  $('#errors').html("I'm sorry, there's been an error. Please try again.")
+}
+function searchRepositories() {
+  const searchTerms = $('#searchTerms').val()
+  $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, data => {
+      const template = Handlebars.compile($('#results-template').html())
+      $('#results').html(template(data))
+    }).fail(error => {
+      displayError()
+    })
+}
+
+function showCommits(el) {
+  const owner = el.dataset.owner
+  const repo = el.dataset.repository
+  $.get(`https://api.github.com/repos/${owner}/${repo}/commits`, data => {
+    //graps the template from html page
+    const template = Handlebars.compile($('#commits-template').html())
+    // gets the
+    $('#details').html(template(data))
+  }).fail(error => {
+    displayError()
+  })
+}
+
 function handlebarsSetup() {
   //put any handlebars setup in here
   Handlebars.registerPartial("userDetails", $("#user-details-partial").html())
+
 }
 
 $(document).ready(function (){
-  handlebarsSetup()
+  // handlebarsSetup()
+
+
+
 });
