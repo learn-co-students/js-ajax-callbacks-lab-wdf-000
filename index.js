ba2b8f2b323fbb30ec565e0054926d7e977d486b
jin501 <soljin01@gmail.com>
2016-12-05 17:32:49 -0500

    Done.

--- index.js
+++ index.js
@@ -4,5 +4,34 @@ function handlebarsSetup() {
 }
 
 $(document).ready(function (){
+  searchRepositories()
   handlebarsSetup()
 });
+
+function searchRepositories() {
+    const searchTerm = $('#searchTerms').val()
+
+    var url = `https://api.github.com/search/repositories?q=${searchTerm}`
+    $.get(url, function(response) {
+      const template = Handlebars.compile($("#results-template").html());
+
+    $('#results').html(template(response))
+  })
+}
+
+function displayError() {
+  $('#errors').append("error")
+}
+
+
+function showCommits(arg) {
+  var owner = arg.dataset.owner;
+  var repo = arg.dataset.repository;
+  $.get(`https://api.github.com/repos/${owner}/${repo}/commits`, function(data) {
+    const template2 = Handlebars.compile($('#commits-template').html());
+    $('#details').html(template2(data))
+  }).fail(error => {
+    displayError()
+  });
+
+}
