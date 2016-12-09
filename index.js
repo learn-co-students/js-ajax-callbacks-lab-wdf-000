b6f50c1915e22a4cc021342dafafb474ea7f85d2
Dmitriy Khait <khaitdmitriy@gmail.com>
2016-12-09 13:07:30 -0500

    Done.

--- index.js
+++ index.js
@@ -4,5 +4,44 @@ function handlebarsSetup() {
 }
 
 $(document).ready(function (){
-  handlebarsSetup()
+  handlebarsSetup();
+
+  $("#search").on("click", searchRepositories)
+  $('#results').on('click', '.commits', function(e) {
+
+      showCommits(this);
+
+     });
+
 });
+
+function searchRepositories(){
+    var terms = $("#searchTerms").val();
+    $.get("https://api.github.com/search/repositories?q=" +  terms, function(data){
+          var source   = $("#results-template").html();
+          var template = Handlebars.compile(source);
+          $("#results").append(template(data));
+
+
+   }).fail(function(error) {
+         displayError("I'm sorry, there's been an error. Please try again.");
+       });
+}
+
+  function displayError(){
+    $("#errors").append("I'm sorry, there's been an error. Please try again.")
+  }
+
+  function showCommits(e){
+    var owner = e.dataset.owner;
+    var repo = e.dataset.repository;
+    var commitsUrl = "https://api.github.com/repos/" + owner + "/" + repo + "/commits";
+    $.get(commitsUrl, function(data) {
+
+        let source = $("#commits-template").html();
+        let template = Handlebars.compile(source);
+        $("#details").html(template(data));
+      }).fail(function(error) {
+            displayError();
+          });
+  }
